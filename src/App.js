c951a749b9834583cbdaabdfdf2c6fa9bf2344c6
Travis Turner <RoboEnigma@gmail.com>
2020-01-30 18:55:59 +0300

    Add nav and segment components

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navigation from './Navigation';
+import Segment from './Segment';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
+
+/* Finish the resume to get the dates right and add the new project */
+/* Just make an about me section and use text from your cover letter */
+/* Include a git hub page and a link to the resume */
+/* Make the segments and have them take text as inputs in the form of paragraphs 1 2 3 4*/
+/* I can use text from the cover letter and the app descriptions */
+/* They should also take pics which should be easily displayed as thumbnails */
+/* Don't forget screen shots from the back end*/
+/* Include app store links */
+
---
+++ src/Navigation.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const Navigation = (props) => {
+  return (
+    <Navbar id="navBarMain" collapseOnSelect expand="lg" bg="light" variant="light">
+      <Navbar.Brand href="/">Richard Turner</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">   
+        <Nav className="mr-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#simplyEnglish">Simply English</Nav.Link>
+            <Nav.Link href="#hiraganaDriller">Hiragana Driller</Nav.Link>
+            <Nav.Link href="#simon2">Simon II</Nav.Link>
+            <Nav.Link href="#infantSchoolApp">Infant School App</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
+
+export default Navigation;
