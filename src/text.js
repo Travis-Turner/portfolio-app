a9e8d1f13df1404ae72e945344b6ba756581b3f5
Travis Turner <RoboEnigma@gmail.com>
2020-02-03 17:26:42 +0300

    Add app store links

--- src/text.js
+++ src/text.js
@@ -13,11 +13,19 @@ const text = {
     simplyEnglish: [
         "I created this app for the company Simply English in Moscow Russia.  The userbase consists of English-speaking teachers adjusting to life in Moscow. As part of their duties, teachers need to travel to multiple schools around Moscow - a city which occupies a massive amount of real estate. The app consolidates several functions into one place.  Teachers are able to access contacts for emergencies, receive their schedule, and order office supplies. The app also allows teachers to easily route to their apartment and clients, with Yandex - the Russian equivilent of Google. Teachers may also check school calendars, and the app also includes an interactive guidebook to help adjust to life in Russia.  The app is reguarly used by around 50 users at any given time.",
         'The Simply English application is available for iPhone and Android and was developed with React-Native, the backend portal was developed with React and Firebase.',
-        'Happily, based upon the success of the first application, I am expected to develop a second iteraction at the conclusion of the 2020 school year.'
+        'Happily, based upon the success of the first application, I am expected to develop a second iteraction at the conclusion of the 2020 school year.',
+        <div className="TextContainer">You can see the app on the Google Play Store, here <a href="https://play.google.com/store/apps/details?id=com.SimplyEnglish.SimplyEnglish&hl=en">here</a>.</div>,
+        <div className="TextContainer">You can see the app on the Apple App Store, here <a href="https://apps.apple.com/us/app/simply-english/id1475056793">here</a>.</div>,
+        'To log in, use the password, "RichardTurnerTest!".'
+
+
+
     ],
     hiraganaDriller: [
         'This is the first app I created.  It is a simple flashcard app, designed to help learners memorize the Japanese hiragana.  I hand drew each character in Sketch, composed the sound effects with GarageBand and commissioned a native speaker to record audio for each character.  It was important for me to be able to integrate sound into this application, as well as audio and visual cues that made the experience gratifying and pleasant.',
-        'The Hiragana Driller application is available for iPhone and was developed with React-Native.'
+        'The Hiragana Driller application is available for iPhone and was developed with React-Native.',
+        <div className="TextContainer">You can see the app on the Apple App Store, here <a href="https://apps.apple.com/us/app/hiragana-driller/id1345285438">here</a>.</div>,
+
     ],
     simon2: [
         'There is an online community for self-taught developers called FreeCodeCamp.  There are several educational programs available through this site  One of the final projects in the Front-End Developer track is to recreate the popular game "Simon".  I decided to go a different way with the project, incorporating more game modes and integrating music and sound, as well as a title sequence which I created.',
